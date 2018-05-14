a0304ba9c39f10b32ee196e9a4bc9ed3ca8bf0b5
Unknown <jasphi168@gmail.com>
2018-05-14 21:14:14 +0800

    complete crawler

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const Xray = require('x-ray');
+const x = Xray();
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html','table > tr',[
+    {
+    No:'td:nth-child(2)',
+    courseName:'td:nth-child(4)',
+    teacher:'td:nth-child(5)',
+    }
+]).write('./dist/course.json');
